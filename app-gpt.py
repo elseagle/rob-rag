3ee9ae70f30419ff3e1c51a71505b75bd39015f0
elseagle <dowolebolu@gmail.com>
2024-03-20 03:07:27 +0100

    chore: add context engine, prompt template, and update chat ui

--- app-gpt.py
+++ app-gpt.py
@@ -1,21 +1,35 @@
 import streamlit as st
-from llama_index.core import VectorStoreIndex, SimpleDirectoryReader
+from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, PromptTemplate
 from llama_index.core.chat_engine.types import ChatMode
 import openai
 from llama_index.llms.openai import OpenAI
+from llama_index.core.memory import ChatMemoryBuffer
 
-# Securely load the OpenAI API key
 openai.api_key = st.secrets["OPENAI_API_KEY"]
 
-# Setting up the page header
-st.header("Insomnia Labs - Roblox")
-st.subheader("Ask a question about Roblox and get an answer from my knowledge base")
+memory = ChatMemoryBuffer.from_defaults(token_limit=1500)
 
-# Initialize the chat message history
+prompt_template = PromptTemplate("""Given the following {query_str}
+
+Your response should prioritize clarity, conciseness, and relevance to the context of your knowledge base. 
+Avoid unnecessary elaboration and focus on delivering value to the user's experience within the event.
+"""
+                                 )
+# page header
+st.header("Maybelline Roblox Activation - FAQ & How-tos")
+st.subheader("Ask me anything about the Maybelline Roblox Activation")
+
+# initialize chat history
 if "messages" not in st.session_state:
     st.session_state['messages'] = [
-        {"role": "assistant", "content": "Hello! I'm your assistant. Ask me anything about Roblox."},
+        {"role": "assistant", "content": "Hello! I'm your Roblox assistant."},
     ]
+
+# Display chat messages
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.write(message["content"])
+
 
 # Function to load and index the data, with caching to avoid reloading on every interaction
 @st.cache_resource(show_spinner=False)
@@ -30,27 +44,30 @@ def load_data():
         st.error(f"Failed to load and index data: {e}")
         return None
 
+
 index = load_data()
 
 # Proceed only if the data is loaded successfully
 if index is not None:
-    chat_engine = index.as_chat_engine(chat_mode=ChatMode.CONDENSE_QUESTION, llm=OpenAI(model="gpt-4-turbo-preview"), verbose=True)
+    chat_engine = index.as_chat_engine(chat_mode=ChatMode.CONTEXT,
+                                       memory=memory,
+                                       llm=OpenAI(model="gpt-4-turbo-preview"),
+                                       verbose=True)
 
-    if prompt := st.text_input("Ask me anything about Roblox"):
+    user_prompt = st.chat_input("Ask Away..")
+    if user_prompt:
         # Add the user's prompt to the message history
-        st.session_state.messages.append({"role": "user", "content": prompt})
-
-        # Display chat messages
-        for message in st.session_state.messages:
-            with st.chat_message(message["role"]):
-                st.write(message["content"])
+        st.session_state.messages.append({"role": "user", "content": user_prompt})
+        # Display the user's prompt
+        with st.chat_message("user"):
+            st.write(user_prompt)
 
         # Generate a new response if the last message is not from the assistant
         if st.session_state.messages[-1]["role"] != "assistant":
             with st.chat_message("assistant"):
                 with st.spinner("Thinking..."):
                     try:
-                        response = chat_engine.chat(prompt)
+                        response = chat_engine.chat(prompt_template.format(query_str=user_prompt))
                         st.write(response.response)
                         # Add the assistant's response to the message history
                         st.session_state.messages.append({"role": "assistant", "content": response.response})
